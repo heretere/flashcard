3b11e1458f4605ebb515ddce3d433165f7345e9f
Heretere <heirteir@protonmail.com>
2021-07-25 07:05:44 -0400

    fixes v2

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -2,7 +2,7 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = "https://my-json-server.typicode.com/";
+const API_BASE_URL = "https://my-json-server.typicode.com/heretere/flashcard";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
